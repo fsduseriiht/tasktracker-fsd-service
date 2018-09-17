7ca2a85d4ceab97438d4a8a40304a6822be2998e
Amitabha Das <fsd,user.iiht@gmail.com>
2018-09-17 16:41:36 +0530

    Changes Related to docker

--- src/main/java/com/cts/fsd/tasktracker/TasktrackerApplication.java
+++ src/main/java/com/cts/fsd/tasktracker/TasktrackerApplication.java
@@ -1,19 +1,43 @@
 package com.cts.fsd.tasktracker;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cts.fsd.tasktracker.test.TestClass;
+
 @SpringBootApplication
 public class TasktrackerApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-//		System.setProperty("key", "value");
 		
 		System.setProperty("server.servlet.context-path", "/tasktrackerbackend");
-		System.setProperty("server.port", "8080");
+//		System.setProperty("server.port", "8080");
+//		final File checkFile = new File("\\tmp\\tasktracker");
+//		System.out.println("checkFile.exists() =  " + checkFile.exists());
+//		System.out.println("Check file path = " + new File(".\\src\\main\\resources\\DB\\tasktracker.mv.db").getAbsolutePath());
+		
+//		final File f = new File(TasktrackerApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+//		String absPath = f.getAbsolutePath();
+//		System.out.println("absPath ===========    " + absPath );
+//		String finalDbPath = "jdbc:h2:file:" + absPath + "\\DB\\tasktracker";
+//		String finalDbPath = "jdbc:h2:file:.\\tasktracker";
+//		String finalDbPath = "jdbc:h2:file:.\\src\\main\\resources\\DB\\tasktracker;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
+		
+//		String content = "Hello World !!";
+//		Files.write(Paths.get("com/cts/fsd/tasktracker/test/Test.txt"), content.getBytes());
+//		String finalDbPath = System.getProperty("spring.datasource.url");
+		
+		
+		String finalDbPath = "jdbc:h2:file:\\tmp\\tasktracker";
+		System.out.println("finalDbPath " + finalDbPath);
+		System.setProperty("spring.datasource.url", finalDbPath);
 		
 		System.setProperty("spring.datasource.name", "task_db");
-		System.setProperty("spring.datasource.url", "jdbc:h2:file:.\\src\\main\\resources\\DB\\tasktracker;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
 		System.setProperty("spring.datasource.username", "SA");
 		System.setProperty("spring.datasource.password", "");
 		System.setProperty("spring.datasource.driverClassName", "org.h2.Driver");
@@ -27,8 +51,6 @@ public class TasktrackerApplication {
 		
 		System.setProperty("spring.h2.console.enabled", "true");
 		System.setProperty("spring.h2.console.path", "/h2-console");
-		
-		
 		
 		SpringApplication.run(TasktrackerApplication.class, args);
 	}
--- src/main/java/com/cts/fsd/tasktracker/test/TestClass.java
+++ src/main/java/com/cts/fsd/tasktracker/test/TestClass.java
@@ -3,15 +3,11 @@
  */
 package com.cts.fsd.tasktracker.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.cts.fsd.tasktracker.pojo.ParentTaskPOJO;
-import com.cts.fsd.tasktracker.pojo.TaskPOJO;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 /**
  * @author Amitabha Das Local
@@ -22,48 +18,11 @@ public class TestClass {
 	/**
 	 * @param args
 	 * @throws ParseException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws ParseException {
-		// TODO Auto-generated method stub
-		Gson gson = new Gson();
-		
-		ParentTaskPOJO parentTaskPOJO = new ParentTaskPOJO();
-		parentTaskPOJO.setParentId(101);
-		parentTaskPOJO.setParentTask("ParentTaskName");
-		
-		TaskPOJO taskPOJO = new TaskPOJO();
-		taskPOJO.setTaskId(1);
-		taskPOJO.setTask("TaskName");
-		taskPOJO.setParentId(101);
-		taskPOJO.setParentTask("ParentTaskName");
-		taskPOJO.setPriority(1);
-		taskPOJO.setStartDate(new SimpleDateFormat("dd/MM/yyyy").parse("09/09/2018"));
-		taskPOJO.setEndDate(new SimpleDateFormat("dd/MM/yyyy").parse("09/09/2018"));
-		
-		
-		String jsonString = gson.toJson(parentTaskPOJO);
-		System.out.println("jsonString = " + jsonString);
-		
-		
-		List<TaskPOJO> taskPOJOList1 = new ArrayList<TaskPOJO>();
-		taskPOJOList1.add(taskPOJO);
-		String jsonString2 = gson.toJson(taskPOJOList1);
-		System.out.println("jsonString2 = " + jsonString2);
-		
-		
-		
-		
-		String parentTaskDump = "[{\"parentId\":101,\"parentTask\":\"ParentTask1\"},{\"parentId\":102,\"parentTask\":\"ParentTask2\"},{\"parentId\":103,\"parentTask\":\"ParentTask3\"},{\"parentId\":104,\"parentTask\":\"ParentTask4\"},{\"parentId\":105,\"parentTask\":\"ParentTask5\"},{\"parentId\":106,\"parentTask\":\"ParentTask6\"},{\"parentId\":107,\"parentTask\":\"ParentTask7\"}]";
-		List<ParentTaskPOJO> parentTaskPOJOList = gson.fromJson(parentTaskDump, new TypeToken<List<ParentTaskPOJO>>(){}.getType());
-		parentTaskPOJOList.forEach((pojo)-> {System.out.println(pojo);});
-		
-//		jsonString2 = jsonString2.replaceAll("\"", "\\\\\"");
-//		System.out.println("jsonString2.replaceAll() = " + jsonString2);
-		
-		
-		String taskDump = "[{\"taskId\":11,\"task\":\"TaskName1\",\"parentId\":101,\"parentTask\":\"ParentTaskName1\",\"startDate\":\"Sep 9, 2018 12:00:00 AM\",\"endDate\":\"Sep 9, 2018 12:00:00 AM\",\"priority\":1},{\"taskId\":12,\"task\":\"TaskName2\",\"parentId\":102,\"parentTask\":\"ParentTaskName2\",\"startDate\":\"Sep 9, 2018 12:00:00 AM\",\"endDate\":\"Sep 9, 2018 12:00:00 AM\",\"priority\":2},{\"taskId\":13,\"task\":\"TaskName3\",\"parentId\":101,\"parentTask\":\"ParentTaskName1\",\"startDate\":\"Sep 9, 2018 12:00:00 AM\",\"endDate\":\"Sep 9, 2018 12:00:00 AM\",\"priority\":3},{\"taskId\":14,\"task\":\"TaskName4\",\"parentId\":103,\"parentTask\":\"ParentTaskName3\",\"startDate\":\"Sep 9, 2018 12:00:00 AM\",\"endDate\":\"Sep 9, 2018 12:00:00 AM\",\"priority\":4},{\"taskId\":15,\"task\":\"TaskName5\",\"parentId\":105,\"parentTask\":\"ParentTaskName5\",\"startDate\":\"Sep 9, 2018 12:00:00 AM\",\"endDate\":\"Sep 9, 2018 12:00:00 AM\",\"priority\":5}]";
-		List<TaskPOJO> taskPOJOList = gson.fromJson(taskDump, new TypeToken<List<TaskPOJO>>(){}.getType());
-		taskPOJOList.forEach((pojo)-> {System.out.println(pojo);});
+	public static void main(String[] args) throws ParseException, IOException {
+//		String content = "Hello World !!";
+//		Files.write(Paths.get("./src/main/java/com/cts/fsd/tasktracker/test/Test.txt"), content.getBytes());
 	}
 
 }
