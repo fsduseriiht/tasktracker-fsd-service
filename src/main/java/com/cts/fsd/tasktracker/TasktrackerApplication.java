87425a9592a6cd990bfa8fb4037a583786d1b3c0
fsduseriiht <fsd.user.iiht@gmail.com>
2018-12-03 19:33:44 +0530

    included updates as per Capsule Program

--- src/main/java/com/cts/fsd/tasktracker/TasktrackerApplication.java
+++ src/main/java/com/cts/fsd/tasktracker/TasktrackerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class TasktrackerApplication {
+public class TasktrackerApplication extends ServletInitializer {
 	public static void main(String[] args) throws IOException {
 		
 //		set application configurations required to run the spring boot application
@@ -27,24 +27,32 @@ public class TasktrackerApplication {
 //		System.setProperty("server.port", "8080");
 		
 //		spring.datasource properties
-		System.setProperty("spring.datasource.url", "jdbc:h2:file:~\\tmp\\tasktracker");
+		System.setProperty("spring.datasource.url", "jdbc:mysql://localhost:3306/fsd_task_tracker?createDatabaseIfNotExist=true");
+		System.setProperty("spring.datasource.username", "root");
+		System.setProperty("spring.datasource.password", "admin");
+		System.setProperty("spring.datasource.driverClassName", "com.mysql.jdbc.Driver");
+		System.setProperty("spring.datasource.initialize", "true");
+		
+		/*System.setProperty("spring.datasource.url", "jdbc:h2:file:~\\tmp\\tasktracker");
 		System.setProperty("spring.datasource.name", "task_db");
 		System.setProperty("spring.datasource.username", "SA");
 		System.setProperty("spring.datasource.password", "");
 		System.setProperty("spring.datasource.driverClassName", "org.h2.Driver");
-		System.setProperty("spring.datasource.initialize", "true");
+		System.setProperty("spring.datasource.initialize", "true");*/
 		
 		
 //		spring.jpa properties
 		System.setProperty("spring.jpa.generate-ddl", "true");
 		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
-		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
-		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQLDialect");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		/*System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");*/
 		System.setProperty("spring.jpa.show-sql", "true");
 		
 //		spring.h2 properties
-		System.setProperty("spring.h2.console.enabled", "true");
-		System.setProperty("spring.h2.console.path", "/h2-console");
+//		System.setProperty("spring.h2.console.enabled", "true");
+//		System.setProperty("spring.h2.console.path", "/h2-console");
 		
 	}
 }
